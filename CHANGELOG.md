4873de5e73001d748d5089ce05a12e0a99a3d5a2
Thomas Roch <thomas.c.roch@gmail.com>
2016-01-05 20:12:07 +0000

    chore: v1.0.1

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,9 @@
+<a name="1.0.1"></a>
+## [1.0.1](https://github.com/troch/route-node/compare/v1.0.0...v1.0.1) (2016-01-05)
+
+
+
+
 <a name="1.0.0"></a>
 # [1.0.0](https://github.com/troch/route-node/compare/v0.6.0...v1.0.0) (2016-01-05)
 
--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,16 @@ import babel from 'rollup-plugin-babel';
 import npm from 'rollup-plugin-npm';
 import { argv } from 'yargs';
 
+const babelOptions = {
+    presets: [ 'es2015-rollup' ],
+    plugins: [
+        'transform-object-rest-spread',
+        'transform-class-properties',
+        'transform-export-extensions'
+    ],
+    babelrc: false
+};
+
 const format = argv.format || 'umd';
 const dest = {
     amd:  'dist/amd/route-node.js',
@@ -11,7 +21,7 @@ const dest = {
 export default {
     entry: 'modules/RouteNode.js',
     format,
-    plugins: [ babel(), npm({ jsnext: true }) ],
+    plugins: [ babel(babelOptions), npm({ jsnext: true }) ],
     moduleName: 'RouteNode',
     moduleId: 'RouteNode',
     dest
